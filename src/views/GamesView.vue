4b6fba41ed4097534ac96aaa00fcab51fcbd0020
hydrarguru <henrik@engqvist.org>
2024-02-05 17:57:07 +0100

    Add multiple Card components to GamesView

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,12 +1,47 @@
 <script setup lang="ts">
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/Footer.vue'
+import Card from '../components/Card.vue'
 </script>
 
 <template>
   <NavBar />
-  <h1>Games View</h1>
+  <main class="container">
+    <Card
+      title="Shoes"
+      description="The best shoes in the world"
+      image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      :tags="['shoes', 'fashion']"
+      />
+      <Card
+      title="Shoes"
+      description="The best shoes in the world"
+      image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      :tags="['shoes', 'fashion']"
+      />
+      <Card
+      title="Shoes"
+      description="The best shoes in the world"
+      image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      :tags="['shoes', 'fashion']"
+      />
+      <Card
+      title="Shoes"
+      description="The best shoes in the world"
+      image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      :tags="['shoes', 'fashion']"
+      />
+  </main>
   <Footer />
 </template>
 
-<style scoped></style>
+<style scoped>
+  .container {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+  }
+  .container > * {
+    margin: 1rem;
+  }
+</style>
