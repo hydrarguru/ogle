20bda222527c7c65fd175d562348845dad0f0448
hydrarguru <henrik@engqvist.org>
2024-02-05 17:04:18 +0100

    Add Footer, NavBar and Hero components

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by hydrarguru</p>
+    </aside>
+  </footer>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <div class="navbar bg-base-100">
+            <a class="btn btn-ghost text-xl">Ogle</a>
+        </div>
+    </nav>
+</template>
+
+<style scoped></style>
