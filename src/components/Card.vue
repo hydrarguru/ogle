d872cafc6dd9262995184ccc1a337992673fbc02
hydrarguru <henrik@engqvist.org>
2024-02-05 17:56:42 +0100

    Add Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+    defineProps({
+      title: String,
+      description: String,
+      image: String,
+      tags: Array,
+    })
+</script>
+
+<template>
+  <div class="card w-96 bg-base-100 shadow-xl">
+    <figure>
+      <img
+        src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        alt="Shoes"
+      />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ title }}</h2>
+      <p>{{ description }}</p>
+      <div class="card-actions justify-end" v-for="tag in tags">
+        <div class="badge badge-outline">{{ tag }}</div>
+      </div>
+    </div>
+  </div>
+</template>
