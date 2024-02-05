892b82b520f77c93de07c8ca512bbf533363bbf3
hydrarguru <henrik@engqvist.org>
2024-02-05 17:25:44 +0100

    Add GamesView component

---
+++ src/views/GamesView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+  <NavBar />
+  <h1>Games View</h1>
+  <Footer />
+</template>
+
+<style scoped></style>
