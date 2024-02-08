a4a551694f2d3677b5c28fe138e9ff2e42326f48
hydrarguru <henrik@engqvist.org>
2024-02-08 14:13:52 +0100

    Add GamesView component and update router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GamesView from '../views/GamesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,8 +13,13 @@ const router = createRouter({
     {
       path: '/games',
       name: 'games',
-      component: () => import('../views/GamesView.vue')
-    }
+      component: GamesView
+    },
+    {
+      path: '/games/:id',
+      name: 'game',
+      component: () => import('../views/HomeView.vue')
+    },
   ]
 })
 
