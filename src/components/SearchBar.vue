8a98c79513a6fa24110bd06699e2fd44cc72d639
hydrarguru <henrik@engqvist.org>
2024-02-08 14:46:46 +0100

    Add SearchBar component and update GamesView layout

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+
+</script>
+<template>
+    <div>
+        <input type="text" placeholder="Search for games" />
+    </div>
+</template>
+
+
+<style scoped>
+    input {
+        color: #4d4d4d;
+        background-color: var(--secondary-color);
+        font-size: 1.5rem;
+
+        width: 100%;
+        padding: 1rem;
+
+        border: none;
+        border-radius: 12px;
+    }
+    input:hover, input:focus {
+        background-color: #f2f2f2;
+        transition: all 500ms ease-in-out;
+    }
+
+</style>
--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,24 +1,39 @@
 <script setup lang="ts">
 import NavBar  from "../components/NavBar.vue";
 import GameCard from "../components/GameCard.vue";
+import SearchBar from "../components/SearchBar.vue";
 </script>
 
 <template>
   <NavBar />
   <main>
-    <h1>Games</h1>
+    <SearchBar />
     <div class="games-container">
       <GameCard
         gameTitle="Cyberpunk 2077"
         gameDescription="Game Description"
-        gameImage="https://via.placeholder.com/250"
+        gameImage="https://placehold.co/250x350"
         gamePrice="19.99"
         gameRating="4.5"
       />
       <GameCard
-        gameTitle="Cyber"
+        gameTitle="Hogwarts Legacy"
         gameDescription="Game Description"
-        gameImage="https://via.placeholder.com/250"
+        gameImage="https://placehold.co/250x350"
+        gamePrice="19.99"
+        gameRating="4.5"
+      />
+      <GameCard
+        gameTitle="Elden Ring"
+        gameDescription="Game Description"
+        gameImage="https://placehold.co/250x350"
+        gamePrice="19.99"
+        gameRating="4.5"
+      />
+      <GameCard
+        gameTitle="Cyberpunk 2077"
+        gameDescription="Game Description"
+        gameImage="https://placehold.co/250x350"
         gamePrice="19.99"
         gameRating="4.5"
       />
@@ -34,8 +49,9 @@ import GameCard from "../components/GameCard.vue";
     flex-direction: column;
   }
   .games-container {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
-  }
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 2rem;
+    margin-top: 2rem;
+  }  
 </style>
