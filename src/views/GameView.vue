76243c28edb95fe877b916ae082ad276d19e9379
hydrarguru <henrik@engqvist.org>
2024-02-17 20:56:07 +0100

    Initial commit of GameView component.

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import NavBar from '../components/NavBar.vue'
+import router from '../router';
+import { onMounted } from 'vue';
+import { fetchGamesStore } from '../stores/gameStore';
+
+const gameId = router.currentRoute.value.params.id;
+const gameStore = fetchGamesStore();
+console.log(gameStore.fetchGameDetails('3498'));
+
+
+onMounted(() => {
+    console.log(gameId);
+})
+
 </script>
 
 <template>
-    <div>
+    <NavBar />
+    <main>
         <h1>Game View</h1>
         <p>{{ $route.params.id }}</p>
-    </div>
+    </main>
 </template>
 
 <style scoped>
