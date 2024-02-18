df42e39877ed5cb73b1150017937da1629ac8668
hydrarguru <henrik@engqvist.org>
2024-02-18 16:25:34 +0100

    feat: Added game details to GameView.

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -3,12 +3,23 @@ import NavBar from '../components/NavBar.vue'
 import router from '../router';
 import { onMounted } from 'vue';
 import { fetchGamesStore } from '../stores/gameStore';
-import { ref } from 'vue';
 
 const gameId = router.currentRoute.value.params.id.toString();
 const gameStore = fetchGamesStore();
-//const gameDetails = await gameStore.fetchGameDetails(gameId);
+const gameDetails = await gameStore.fetchGameDetails(gameId);
+console.log(gameDetails);
 
+const gamePlatforms = gameDetails.parent_platforms.map((platform) => {
+    return platform.platform.name;
+}).join(', ');
+
+const gameGenres = gameDetails.genres.map((genre) => {
+    return genre.name;
+}).join(', ');
+
+const gameTags = gameDetails.tags.map((tag) => {
+    return tag.name;
+}).join(', ');
 
 
 onMounted(() => {
@@ -20,20 +31,29 @@ onMounted(() => {
 <template>
     <NavBar />
     <main>
+        <section class="container">
+            <h1>{{ gameDetails.name }}</h1>
+            <img :src="gameDetails.background_image" alt="Grand Theft Auto V">
+            
+        </section>
         <div class="grid-container">
             <section class="container">
-                <h1>Grand Theft Auto V</h1>
-                <img src="https://via.placeholder.com/800x400" alt="Grand Theft Auto V">
-                
+                <h1>Game Details</h1>
+                <ol>
+                    <li>Platform: {{ gamePlatforms }}</li>
+                    <li>Release Date: {{ gameDetails.released }}</li>
+                    <li>Developer: {{ gameDetails.developers[0].name }}</li>
+                    <li>Publisher: {{ gameDetails.publishers[0].name }}</li>
+                    <li>Genre: {{ gameGenres }}</li>
+                    <li>Tags: {{ gameTags }}</li>
+                </ol>
             </section>
             <section class="container">
-                <h1>Game Details</h1>
-                <p>User ratings</p>
-            </section>
-            
-            <section class="container">
-                <h1>Game Details</h1>
-                <p>User ratings</p>
+                <h1>Game Ratings</h1>
+                <ol>
+                    <li>Metacritic: {{ gameDetails.metacritic }}</li>
+                    <li>Community Rating: {{ gameDetails.rating }}/5</li>
+                </ol>
             </section>
         </div>
     </main>
@@ -47,8 +67,10 @@ onMounted(() => {
     }
     .grid-container {
         display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-template-rows: 1fr 1fr;
+        grid-template-columns: 1fr 1fr 1fr;
+        align-items: flex-start;
+        padding-left: 2rem;
+        padding-right: 2rem;
     }
     .container {
         display: flex;
@@ -62,6 +84,13 @@ onMounted(() => {
         padding: 1em;
         margin: 1em;
     }
+
+    .container li {
+        list-style-type: armenian;
+        padding: 0;
+        text-decoration: dotted;
+    }
+
     .container * {
         margin: 0.5rem;
         padding: 0.25rem;
