0c1689706f01de1834d4350242bf85861b6c7acd
hydrarguru <henrik@engqvist.org>
2024-02-19 18:45:15 +0100

    Add FooterComponent.vue

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        <p>
+            2024 &copy; Oogle. All rights reserved.
+            Data & Images provided by: <a href="https://rawg.io/">RAWG</a>
+        </p>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        margin-top: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 6rem;
+
+        background-color: var(--secondary-color);
+        box-shadow: 0 10px 20px 0 rgba(0,0,0,.07);
+    }
+    p { 
+        text-align: center;
+        font-style: italic;
+    }
+    a {
+        color: var(--accent);
+        text-decoration: none;
+    }
+</style>
