4062e9e7dc58c3c9c4399b87fdf8a9c3d1c0fe6b
hydrarguru <henrik@engqvist.org>
2024-02-19 21:54:41 +0100

    Refactor search functionality and add search icon

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,27 +1,62 @@
 <script lang="ts">
+import { MagnifyingGlassIcon } from "@heroicons/vue/24/outline";
+
 export default {
     setup() {
         return {
-            searchInput: ''
-        }
+            searchInput: '',
+        };
     },
     methods: {
         searchGames() {
-            alert(this.searchInput);
+            let modifiedArray = this.gameArray;
+            modifiedArray = modifiedArray.filter((game: any) => {
+                return game.name.toLowerCase().includes(this.searchInput.toLowerCase());
+            });
         }
-    }
+    },
+    props: {
+        gameArray: Array
+    },
+
 }
 
 
 </script>
 <template>
-    <div>
-        <input @keyup.enter="searchGames()" v-model="searchInput" type="text" placeholder="Search for games"/>
+    <div class="wrapper">
+        <input v-model="searchInput" type="text" placeholder="Search for games"/>
+        <button @click="searchGames()">
+            <MagnifyingGlassIcon class="icon"></MagnifyingGlassIcon>
+        </button>
     </div>
 </template>
 
 
 <style scoped>
+    .wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    button {
+        color: #4d4d4d;
+        background-color: var(--secondary-color);
+        font-size: 1.5rem;
+
+        padding: 1rem;
+        margin-left: 1rem;
+
+        border: none;
+        border-radius: 12px;
+    }
+
+    button:hover {
+        background-color: #f2f2f2;
+        transition: all 500ms ease-in-out;
+    }
+
     input {
         color: #4d4d4d;
         background-color: var(--secondary-color);
@@ -39,4 +74,14 @@ export default {
         transition: all 500ms ease-in-out;
     }
 
+    .icon {
+        color: #ffffff;
+        width: 24px;
+        height: 24px;
+        justify-content: center;
+    }
+    button + .icon:hover {
+        transition: all 300ms ease-in-out;
+        color: #000000
+    }
 </style>
