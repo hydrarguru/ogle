c04f8dd684cc8ee308be701e69c8046ee47fc8ed
hydrarguru <henrik@engqvist.org>
2024-02-19 18:04:10 +0100

    Refactor fetchGames function to accept page and pageSize parameters

--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -4,8 +4,8 @@ import { defineStore } from 'pinia'
 export const fetchGamesStore = defineStore('fetchGameStore', () => {
     const gamesList = ref([])
 
-    async function fetchGames() {
-        const response = await fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=1&page_size=10`);
+    async function fetchGames(page: number = 1, pageSize: number = 10) {
+        const response = await fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=${page}&page_size=${pageSize}`);
         const data = await response.json();
         return data.results;
     }
