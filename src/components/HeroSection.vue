7740b06a977e25a8b0284b62377718c51f3d5ee2
hydrarguru <henrik@engqvist.org>
2024-02-21 17:00:32 +0100

    Refactor HeroSection.vue component

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,23 +1,46 @@
 <script setup lang="ts">
+defineProps({
+    backgroundColor: {
+        type: String
+    },
+    elementAlignment: {
+        type: String
+    },
+    sectionTitle: {
+        type: String,
+        default: 'Section Title'
+    },
+    sectionText: {
+        type: String,
+        default: 'Section Text'
+    }
+})
 
 </script>
 
 <template>
-    <section class="hero-container">
-        <div class="hero-text">
-            <h1>Game Store</h1>
-            <p>Get the latest games at the best prices</p>
+    <section class="hero-container" :style="{ backgroundColor: backgroundColor, alignItems: elementAlignment }">
+        <div class="hero-text-container">
+            <h1>{{ sectionTitle }}</h1>
+            <p>{{ sectionText }}</p>
         </div>
+        <aside>
+            <img src="https://placehold.co/600x400" alt="Placeholder image" />
+        </aside>
     </section>
 </template>
 
 <style scoped>
     .hero-container {
-        background-color: #f2f2f2;
-        text-align: center;
+        display: flex;
     }
 
-    .hero-text {
-        color: #333;
+    .hero-text-container {
+        text-align: center;
+        padding: 2rem;
+    }
+
+    .hero-container h1 p {
+        color: #ffffff;
     }
 </style>
