b49cc4969bc0354fbd13b38cf7d4c589298c7fcc
hydrarguru <henrik@engqvist.org>
2024-02-21 17:00:22 +0100

    Update HeroSection in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,24 @@ import HeroSection from '../components/HeroSection.vue'
 
 <template>
   <main>
-    <HeroSection />
+    <HeroSection
+      backgroundColor="#151515" 
+      elementAlignment="flex-start"
+      sectionTitle="Extensive library of games to browse."
+      sectionText="Browse our extensive library of 500,000+ games to find your next game."
+    />
+    <HeroSection
+      backgroundColor="#202020"
+      elementAlignment="center"
+      sectionTitle="Personalized collections"
+      sectionText="Create collections of your favorite games to share with friends or save for later."
+    />
+    <HeroSection
+      backgroundColor="#151515"
+      elementAlignment="flex-end"
+      sectionTitle="Accounts & Community"
+      sectionText="Create an account to join our community and start sharing your collections."
+    />
   </main>
 </template>
 
