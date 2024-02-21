0393e0dac9fc02251e066a0c04f2be8edcd225f5
hydrarguru <henrik@engqvist.org>
2024-02-21 16:57:45 +0100

    Refactor fetchGames function to handle loading state and error handling

--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -6,10 +6,16 @@ export const fetchGamesStore = defineStore('fetchGameStore', () => {
 
     async function fetchGames(page: number = 1, pageSize: number = 10) {
         isLoading.value = true;
-        const response = await fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=${page}&page_size=${pageSize}`);
-        const data = await response.json();
-        isLoading.value = false;
-        return data.results;
+        console.log(isLoading.value)
+        try {
+            const response = await fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=${page}&page_size=${pageSize}`);
+            const data = await response.json();
+            return data.results;
+        }
+        finally {
+            isLoading.value = false;
+            console.log(isLoading.value)
+        }
     }
 
     async function fetchGameDetails(id: string) {
