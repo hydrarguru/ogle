310aa42ec6a5af8d82e264afa7596a303852fbc3
hydrarguru <henrik@engqvist.org>
2024-02-21 14:34:26 +0100

    Add HeroSection component

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <section class="hero-container">
+        <div class="hero-text">
+            <h1>Game Store</h1>
+            <p>Get the latest games at the best prices</p>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    .hero-container {
+        background-color: #f2f2f2;
+        text-align: center;
+    }
+
+    .hero-text {
+        color: #333;
+    }
+</style>
