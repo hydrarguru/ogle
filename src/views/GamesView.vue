78e5f82b953222a548408dae2dea0190f3700b67
hydrarguru <henrik@engqvist.org>
2024-07-21 19:23:04 +0200

    refactor(GamesView): responsive design.
    Added media queries for mobile, mobile landscape, tablet, tablet landscape & desktop.

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import GameCard from "../components/GameCard.vue";
+import PaginationComponent from "../components/PaginationComponent.vue";
 import { fetchGamesStore } from "../stores/gameStore";
 
 const gameStore = fetchGamesStore();
@@ -15,6 +16,7 @@ const gameItems = await gameStore.fetchGames(1, 25);
       </article>
     </section>
   </main>
+    <PaginationComponent />
 </template>
 
 <style scoped>
@@ -32,5 +34,41 @@ const gameItems = await gameStore.fetchGames(1, 25);
     grid-auto-rows: 1fr;
     grid-column-gap: 1rem;
     grid-row-gap: 1rem;
+  }
+
+  @media (max-width: 360px) {
+    .games-container {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+
+  @media (min-width: 361px) and (max-width: 666px) {
+    .games-container {
+      grid-template-columns: repeat(1, 1fr);
+    }
+  }
+
+  @media (min-width: 667px) and (max-width: 768px) {
+    .games-container {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    .games-container {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  @media (min-width: 1025px) and (max-width: 1300px) {
+    .games-container {
+      grid-template-columns: repeat(5, 1fr);
+    }
+  }
+
+  @media (min-width: 1301px) {
+    .games-container {
+      grid-template-columns: repeat(5, 1fr);
+    }
   }
 </style>
