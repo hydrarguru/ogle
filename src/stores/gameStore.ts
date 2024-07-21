9175c5f318fdd433424ba000415806b8a9dedc10
hydrarguru <henrik@engqvist.org>
2024-07-21 18:34:46 +0200

    refactor: gameStore error handling.

--- src/stores/gameStore.ts
+++ src/stores/gameStore.ts
@@ -1,18 +1,13 @@
-import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const fetchGamesStore = defineStore('fetchGameStore', () => {
-    const isLoading = ref(false)
-
     async function fetchGames(page: number = 1, pageSize: number = 10) {
-        isLoading.value = true;
         try {
             const response = await fetch(`https://api.rawg.io/api/games?key=${import.meta.env.VITE_API_KEY}&page=${page}&page_size=${pageSize}`);
             const data = await response.json();
             return data.results;
-        }
-        finally {
-            isLoading.value = false;
+        } catch (error) {
+            console.error(error);
         }
     }
 
@@ -22,5 +17,5 @@ export const fetchGamesStore = defineStore('fetchGameStore', () => {
         return data;
     }
 
-    return { isLoading, fetchGames, fetchGameDetails }
+    return { fetchGames, fetchGameDetails }
 })
